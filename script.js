bb2370ba082f8d2dd945f92cacdfda91e3f594f3
Pedro Henrique Boccardo de Abreu <pedrohenriqueboccardodeabreu@gmail.com>
2025-05-13 20:54:42 -0300

    atualizações de sistema

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    
     function ajustaAltura() {
         const boxes = document.querySelectorAll(".box");
         boxes.forEach(function (box) {
@@ -22,3 +24,47 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+let player = 0;
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+  box.addEventListener("click", () => {
+    const icon = box.querySelector('i');
+
+    if (icon && !icon.classList.contains('fa-close') && !icon.classList.contains('fa-circulo')) {
+      if (player === 0) {
+        icon.classList.add('fa', 'fa-close');
+        testaClasses();
+        player = 1;
+      } else {
+        icon.classList.add('fa', 'fa-circulo');
+        testaClasses();
+        player = 0;
+      }
+    }
+  });
+});
+    let combox = "";
+    let comboo = "";
+function testaClasses() {
+    
+    combox = "";
+    const elements = document.querySelectorAll(".fa-close");
+    elements.forEach(el => {
+    combox += el.id; // ou el.getAttribute("id") — ambos funcionam
+    });
+
+    
+}
+
+// Botão de reiniciar
+document.getElementById("botao").addEventListener("click", function() {
+  const icons = document.querySelectorAll(".box i");
+  
+  icons.forEach(icon => {
+     icon.classList.remove("fa", "fa-close");
+     icon.classList.remove("fa", "fa-circulo");
+  });
+  combox = "";
+  comboo = "";
+  player = 0;
+});
